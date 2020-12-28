ec8c5c5d88aa7aad0006e016862f57eaaaba7a04
Nick Thomas <nichoth@gmail.com>
2020-12-27 16:48:46 -0800

    add config file

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+// 11ty configuration
+module.exports = config => {
+    // 11ty defaults
+    return {
+        dir: {
+            input: 'src',
+            output: 'public'
+        }
+    };
+};
+
